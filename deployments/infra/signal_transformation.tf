acf8bb66ed5c922ad281a912e8e79a43c5d8720c
Josh Rolstad <jrolstad@salesforce.com>
2022-04-13 12:59:07 -0700

    Adding signal transformation lambda

---
+++ deployments/infra/signal_transformation.tf
@@ -0,0 +1,30 @@
+resource "aws_lambda_function" "signal_persistance" {
+  function_name = "${local.service_name}_signal_persistance"
+
+  role = aws_iam_role.lambda_exec.arn
+
+  image_uri    = "${aws_ecr_repository.registry.repository_url}:signal_persistance-12"
+  package_type = "Image"
+  
+}
+
+resource "aws_cloudwatch_log_group" "signal_persistance" {
+  name = "/aws/lambda/${aws_lambda_function.signal_persistance.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_sns_topic_subscription" "signal_persistance" {
+  topic_arn = aws_sns_topic.signal_received.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.signal_persistance.arn
+}
+
+resource "aws_lambda_permission" "signal_persistance" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.signal_persistance.arn
+  principal     = "sns.amazonaws.com"
+  statement_id  = "AllowSubscriptionToSNS"
+  source_arn    = aws_sns_topic.signal_received.arn
+}
+
