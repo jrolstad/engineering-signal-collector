a67ba836e6357bcb1e380faf32b79ea9ac90fd7e
Josh Rolstad <jrolstad@salesforce.com>
2022-04-12 20:39:35 -0700

    using most recent github agent

--- deployments/infra/agent_github.tf
+++ deployments/infra/agent_github.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "agent_github" {
 
   role = aws_iam_role.lambda_exec.arn
 
-  image_uri    = "${aws_ecr_repository.registry.repository_url}:agent_github-3"
+  image_uri    = "${aws_ecr_repository.registry.repository_url}:agent_github-4"
   package_type = "Image"
   
 }
