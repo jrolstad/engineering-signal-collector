c8b50a0cfa773787eebbd098fd88e57e66428103
Josh Rolstad <jrolstad@salesforce.com>
2022-04-13 09:34:23 -0700

    Adding signal peristance lambda

---
+++ deployments/infra/signal_persistance.tf
@@ -0,0 +1,20 @@
+resource "aws_lambda_function" "signal_persistance" {
+  function_name = "${local.service_name}_signal_persistance"
+
+  role = aws_iam_role.lambda_exec.arn
+
+  image_uri    = "${aws_ecr_repository.registry.repository_url}:persistance-9"
+  package_type = "Image"
+  
+}
+
+resource "aws_cloudwatch_log_group" "persistance" {
+  name = "/aws/lambda/${aws_lambda_function.persistance.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_event_source_mapping" "persistance" {
+  event_source_arn = aws_sns_topic.signal_received.arn
+  function_name    = aws_lambda_function.persistance.arn
+}
