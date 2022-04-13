2f7059151158d13b78fb1da84c4d7977cc396ad9
Josh Rolstad <jrolstad@salesforce.com>
2022-04-12 20:23:13 -0700

    Creating policy for sending

--- deployments/infra/messaging.tf
+++ deployments/infra/messaging.tf
@@ -2,3 +2,20 @@ resource "aws_sqs_queue" "signal_ingestion" {
   name                  = "${local.service_name}_signal_ingestion"
   fifo_queue            = false
 }
+
+resource "aws_sqs_queue_policy" "signal_ingestion_send" {
+  queue_url = aws_sqs_queue.signal_ingestion.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+              "sqs:SendMessage"
+            ],
+            "Resource": "${aws_sqs_queue.signal_ingestion.arn}"
+        }
+    ]
+  })
+}
