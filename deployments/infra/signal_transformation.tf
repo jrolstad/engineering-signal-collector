7917e6ac02a1084bdd198933819d6a694e23cbe4
Josh Rolstad <jrolstad@salesforce.com>
2022-04-13 13:16:15 -0700

    Adding transformation

--- deployments/infra/signal_transformation.tf
+++ deployments/infra/signal_transformation.tf
@@ -1,28 +1,28 @@
-resource "aws_lambda_function" "signal_persistance" {
-  function_name = "${local.service_name}_signal_persistance"
+resource "aws_lambda_function" "signal_transformation" {
+  function_name = "${local.service_name}_signal_transformation"
 
   role = aws_iam_role.lambda_exec.arn
 
-  image_uri    = "${aws_ecr_repository.registry.repository_url}:signal_persistance-12"
+  image_uri    = "${aws_ecr_repository.registry.repository_url}:signal_transformation-12"
   package_type = "Image"
   
 }
 
-resource "aws_cloudwatch_log_group" "signal_persistance" {
-  name = "/aws/lambda/${aws_lambda_function.signal_persistance.function_name}"
+resource "aws_cloudwatch_log_group" "signal_transformation" {
+  name = "/aws/lambda/${aws_lambda_function.signal_transformation.function_name}"
 
   retention_in_days = 30
 }
 
-resource "aws_sns_topic_subscription" "signal_persistance" {
+resource "aws_sns_topic_subscription" "signal_transformation" {
   topic_arn = aws_sns_topic.signal_received.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.signal_persistance.arn
+  endpoint  = aws_lambda_function.signal_transformation.arn
 }
 
-resource "aws_lambda_permission" "signal_persistance" {
+resource "aws_lambda_permission" "signal_transformation" {
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.signal_persistance.arn
+  function_name = aws_lambda_function.signal_transformation.arn
   principal     = "sns.amazonaws.com"
   statement_id  = "AllowSubscriptionToSNS"
   source_arn    = aws_sns_topic.signal_received.arn
