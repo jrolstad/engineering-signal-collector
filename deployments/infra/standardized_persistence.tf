b9aa83769c791203d86f00f8849cb4fc895060d9
Josh Rolstad <jrolstad@salesforce.com>
2022-04-13 13:12:15 -0700

    adding standardized persitence

---
+++ deployments/infra/standardized_persistence.tf
@@ -0,0 +1,30 @@
+resource "aws_lambda_function" "standardized_persistance" {
+  function_name = "${local.service_name}_standardized_persistance"
+
+  role = aws_iam_role.lambda_exec.arn
+
+  image_uri    = "${aws_ecr_repository.registry.repository_url}:standardized_persistance-13"
+  package_type = "Image"
+  
+}
+
+resource "aws_cloudwatch_log_group" "standardized_persistance" {
+  name = "/aws/lambda/${aws_lambda_function.standardized_persistance.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_sns_topic_subscription" "standardized_persistance" {
+  topic_arn = aws_sns_topic.signal_standardized.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.standardized_persistance.arn
+}
+
+resource "aws_lambda_permission" "standardized_persistance" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.standardized_persistance.arn
+  principal     = "sns.amazonaws.com"
+  statement_id  = "AllowSubscriptionToSNS"
+  source_arn    = aws_sns_topic.signal_standardized.arn
+}
+
