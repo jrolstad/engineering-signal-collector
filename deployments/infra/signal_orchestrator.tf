4c3b625b1cdf6bfd29dcd357866c095d08673327
Josh Rolstad <jrolstad@salesforce.com>
2022-04-13 12:38:49 -0700

    updating versions

--- deployments/infra/signal_orchestrator.tf
+++ deployments/infra/signal_orchestrator.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "signal_orchestrator" {
 
   role = aws_iam_role.lambda_exec.arn
 
-  image_uri    = "${aws_ecr_repository.registry.repository_url}:signal_orchestrator-9"
+  image_uri    = "${aws_ecr_repository.registry.repository_url}:signal_orchestrator-12"
   package_type = "Image"
   
 }
