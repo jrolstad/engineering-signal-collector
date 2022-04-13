b2cfb6830af4f987121efdbfa67120a0351e4965
Josh Rolstad <jrolstad@salesforce.com>
2022-04-13 13:06:35 -0700

    Adding lambda for standardized persistence

--- internal/pkg/data/buckets.go
+++ internal/pkg/data/buckets.go
@@ -1,6 +1,7 @@
 package data
 
 const (
-	Blob_engineeringsignal_data     = "engineeringsignalcollectorprddata"
-	Directory_engineeringsignal_raw = "signal_raw"
+	Blob_engineeringsignal_data              = "engineeringsignalcollectorprddata"
+	Directory_engineeringsignal_raw          = "signal_raw"
+	Directory_engineeringsignal_standardized = "signal_standardized"
 )
--- internal/pkg/orchestration/signal_persistence.go
+++ internal/pkg/orchestration/signal_persistence.go
@@ -9,3 +9,8 @@ func SaveSignal(blobService data.BlobService, event *models.SignalEvent) error {
 	saveError := blobService.Save(event, data.Blob_engineeringsignal_data, data.Directory_engineeringsignal_raw)
 	return saveError
 }
+
+func SaveStandardizedModel(blobService data.BlobService, event *models.SignalEvent) error {
+	saveError := blobService.Save(event, data.Blob_engineeringsignal_data, data.Directory_engineeringsignal_standardized)
+	return saveError
+}
