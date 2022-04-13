fe148b31d3963dec2cddf0209a8d5dcfdb76abc4
Josh Rolstad <jrolstad@salesforce.com>
2022-04-12 21:12:43 -0700

    Giving all access until I can figure it out

--- deployments/infra/lambda_base.tf
+++ deployments/infra/lambda_base.tf
@@ -20,6 +20,11 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_role_policy_attachment" "admin_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
 resource "aws_apigatewayv2_api" "lambda" {
   name          = local.service_name
   protocol_type = "HTTP"
