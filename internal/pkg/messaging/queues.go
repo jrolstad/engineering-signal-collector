b5e71e436f3a815e77be7ac1a30784190989be4b
Josh Rolstad <jrolstad@salesforce.com>
2022-04-13 15:45:21 -0700

    sending to correct topic

--- internal/pkg/messaging/queues.go
+++ internal/pkg/messaging/queues.go
@@ -5,5 +5,5 @@ const (
 
 	Topic_engineeringsignal_raw            = "engineering_signal_collector_prd_signal_received"
 	Topic_engineeringsignal_standardized   = "engineering_signal_collector_prd_signal_standardized"
-	Topic_engineeringsignal_policymeasured = "engineering_signal_collector_prd_signal_policy_measured"
+	Topic_engineeringsignal_policymeasured = "engineering_signal_collector_prd_policy_measured"
 )
